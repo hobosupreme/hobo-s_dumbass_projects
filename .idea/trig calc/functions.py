cc7fa6a4a5b3b42c73912ad27752a2ee08c3689c
hobosupreme <calebpbeng@gmail.com>
2026-05-12 16:18:22 -0400

    fuck

--- .idea/trig calc/functions.py
+++ .idea/trig calc/functions.py
@@ -1,7 +1,23 @@
 import random
+import math
 import tkinter as tk
 
 
 def usrin():
     num = tkusrin.get()
     return num
+
+
+def tan():
+    x = math.tan(usrin())
+    return x
+
+
+def cos():
+    x = math.cos(usrin())
+    return x
+
+
+def sin():
+    x = math.sin(usrin())
+    return x
--- .idea/trig calc/root.py
+++ .idea/trig calc/root.py
@@ -5,16 +5,25 @@ root.title('trig calculator_main')
 
 
 def clear(event):
-    if tkusrin.get() == 'input pasword':
+    if tkusrin.get() == 'input':
         tkusrin.delete(0, tk.END)
         tkusrin.config(fg='black')
 
 
 tkusrin = tk.Entry(root, )
-
-tkusrin.insert(0, 'input pasword')
+tkusrin.insert(0, 'input')
 tkusrin.bind("<FocusIn>", clear)
 tkusrin.pack()
+
+
+tan = tk.Button(root, text='tan', )
+tan.pack(side='bottom', expand=True)
+
+cos = tk.Button(root, text='cos', )
+cos.pack(side='bottom', expand=True)
+
+sin = tk.Button(root, text='sin', )
+sin.pack(side='bottom', expand=True)
 
 
 root.mainloop()
